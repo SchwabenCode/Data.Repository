40f1f2557ef1ba788960fdf5998b71d6c1ea1123
Benjamin Abt <mail@benjamin-abt.com>
2015-10-19 00:49:46 +0200

    # updated interfaces

---
+++ SchwabenCode.Data.Repository/IGenericRepositoryAsync.cs
@@ -0,0 +1,46 @@
+﻿using SchwabenCode.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchwabenCode.Data.Repository
+{
+
+    public interface IRepositoryAsync<TEntity, TIdentifier>
+        where TEntity : IEntity<TIdentifier>
+        where TIdentifier : struct
+    {
+
+        /// <summary>
+        /// Adds given entity
+        /// </summary>
+        Task<TEntity> AddAsync( TEntity entity );
+
+        /// <summary>
+        /// Updates given entity
+        /// </summary>
+        Task<TEntity> UpdateAsync( TEntity entity );
+
+        /// <summary>
+        /// Removes given entity
+        /// </summary>
+        Task<TEntity> RemoveAsync( TEntity entity );
+
+        /// <summary>
+        /// Returns the item with specified identifier
+        /// </summary>
+        Task<TEntity> GetAsync( TIdentifier id );
+
+        /// <summary>
+        /// Returns all items of current collection
+        /// </summary>
+        /// <remarks>Handle with care</remarks>
+        Task<IQueryable<TEntity>> GetAllAsync();
+
+        /// <summary>
+        /// Returns count of all items in current collection
+        /// </summary>
+        Task<long> CountAsync();
+    }
+}
---
+++ SchwabenCode.Data.Repository/IRepository.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using SchwabenCode.Data.Entity;
+using System.Linq;
+
+namespace SchwabenCode.Data.Repository
+{
+    /// <summary>
+    /// Interface for base repository
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    public interface IRepository<TEntity, TIdentifier>
+        where TEntity : IEntity<TIdentifier>
+        where TIdentifier : struct
+    {
+        /// <summary>
+        /// Adds given entity
+        /// </summary>
+        TEntity Add( TEntity entity );
+
+        /// <summary>
+        /// Updates given entity
+        /// </summary>
+        TEntity Update( TEntity entity );
+
+        /// <summary>
+        /// Removes given entity
+        /// </summary>
+        TEntity Remove( TEntity entity );
+
+        /// <summary>
+        /// Returns the item with specified identifier
+        /// </summary>
+        TEntity Get( TIdentifier id );
+
+        /// <summary>
+        /// Returns all items of current collection
+        /// </summary>
+        /// <remarks>Handle with care</remarks>
+        IQueryable<TEntity> GetAll();
+
+        /// <summary>
+        /// Returns count of all items in current collection
+        /// </summary>
+        long Count();
+    }
+
+}
