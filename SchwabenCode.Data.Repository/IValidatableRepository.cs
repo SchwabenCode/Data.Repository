16cfabf14b7d6a9b049b1dca25b89b1ebb837d8d
Benjamin Abt <mail@benjamin-abt.com>
2015-10-19 22:14:27 +0200

    + added repositories with auto validation

---
+++ SchwabenCode.Data.Repository/IValidatableRepository.cs
@@ -0,0 +1,18 @@
+﻿using SchwabenCode.Data.Entity;
+using System.Linq;
+
+namespace SchwabenCode.Data.Repository
+{
+    /// <summary>
+    /// Interface for base repository with auto entity validation
+    /// </summary>
+    /// <typeparam name="TEntity">Entity</typeparam>
+    /// <typeparam name="TIdentifier">Type of entity identifier</typeparam>
+    public interface IValidatableRepository<TEntity, TIdentifier> : IRepository<TEntity, TIdentifier>
+        where TEntity : IValidateableEntity<TIdentifier>
+        where TIdentifier : struct
+    {
+
+    }
+
+}
---
+++ SchwabenCode.Data.Repository/IValidatableRepositoryAsync.cs
@@ -0,0 +1,19 @@
+﻿using SchwabenCode.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchwabenCode.Data.Repository
+{
+    /// <summary>
+    /// Interface for base repository with auto entity validation
+    /// </summary>
+    /// <typeparam name="TEntity">Entity</typeparam>
+    /// <typeparam name="TIdentifier">Type of entity identifier</typeparam>
+    public interface IValidatableRepositoryAsync<TEntity, TIdentifier> : IRepositoryAsync<TEntity, TIdentifier>
+        where TEntity : IEntity<TIdentifier>
+        where TIdentifier : struct
+    {
+
+
+    }
+}
