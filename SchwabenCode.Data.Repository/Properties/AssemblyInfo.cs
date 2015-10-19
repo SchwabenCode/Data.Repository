2356584d54579c91e8d216e348dcf43daaeb53a4
Benjamin Abt <mail@benjamin-abt.com>
2015-10-19 16:59:20 +0200

    # updated version information

--- SchwabenCode.Data.Repository/Properties/AssemblyInfo.cs
+++ SchwabenCode.Data.Repository/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SchwabenCode.Data.Repository")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription( "Base Repository by SchwabenCode" )]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany( "Benjamin Abt (SchwabenCode.com)" )]
 [assembly: AssemblyProduct("SchwabenCode.Data.Repository")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright( "Benjamin Abt (SchwabenCode.com) 2015" )]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion( "0.0.1" )]
+[assembly: AssemblyInformationalVersion( "0.0.1-alpha" )]
+[assembly: AssemblyFileVersion("0.0.0.1")]
